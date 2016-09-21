e34a5d75ea448672a109d349d2459b946f9b37a4
MingwangLin <aacheve0@gmail.com>
2016-09-21 18:19:47 +0800

    test: added legend of live chart

--- app/static/js/cpu.js
+++ app/static/js/cpu.js
@@ -59,10 +59,12 @@ var cpuLoadLive = function (data, $target) {
         var barChartData = {
             labels: label,
             datasets: [{
+                label: "Label 1",
                 fillColor: "#a9cef2",
                 strokeColor: "#7cb5ec",
                 data: cpuload,
             }, {
+                label: "Label 2",
                 fillColor: "#959598",
                 strokeColor: "#46464b",
                 data: cpuload,
@@ -78,6 +80,8 @@ var cpuLoadLive = function (data, $target) {
             scaleStepWidth: 10,
             scaleStartValue: 0
         });
+        $target[0].innerHTML = barCpuChartDemo.generateLegend();
+
     } else {
         log('请求失败');
     }
