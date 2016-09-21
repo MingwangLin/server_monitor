a56da313e0fd762fc1cf34d843803503747750ae
MingwangLin <aacheve0@gmail.com>
2016-09-21 11:16:00 +0800

    debug 'added RAM load chart'

--- test.py
+++ test.py
@@ -79,13 +79,13 @@ def find_all_docments():
 
 
 def main():
-    # db.cpu.delete_many({})
-    ram_info = ram_info_output()
-    cpu_info = cpu_info_output()
-    log(cpu_info)
-    log(ram_info)
-    save_ram_info(ram_info)
-    save_cpu_info(cpu_info)
+    db.cpu.delete_many({})
+    # ram_info = ram_info_output()
+    # cpu_info = cpu_info_output()
+    # log(cpu_info)
+    # log(ram_info)
+    # save_ram_info(ram_info)
+    # save_cpu_info(cpu_info)
 
     # find_all_docments()
 
