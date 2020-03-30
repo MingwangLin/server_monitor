6e678c4341da845eb48677d4dfc0bae38162010a
Mingwang Lin <linmingwang@ata.net.cn>
2020-03-30 14:23:53 +0800

    update wsgi

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,3 @@ from server_monitor import init_app
 
 app = init_app()
 app.debug = True
-gunicorn_error_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers = gunicorn_error_logger.handlers
-app.logger.setLevel(gunicorn_error_logger.level)
